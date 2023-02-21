a09cf4464ba4e21af4005128a773a7f6a2c3e951
Suman Kisku <sumankisku1@gmail.com>
2023-02-21 12:03:27 +0530

    check testcases

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your code here
+const gameContainer = document.getElementById("gameContainer");
+
+
+// render all 1600 pixels
+for(let i = 1; i <= 1600; i++) {
+    let pixel = document.createElement("div");
+    pixel.setAttribute("id", `pixel${i}`);
+    pixel.setAttribute("class", "pixel");
+    gameContainer.appendChild(pixel);
+}
+
+// food
+function renderFood() {
+
+    let food = document.createElement("div");
+    food.setAttribute("class", "food");
+    let id = "pixel" + Math.floor(Math.random() * (1600 - 1) + 1);
+    food.setAttribute("id", id);
+    gameContainer.appendChild(food);
+}
+
+renderFood();
